4601baffa304f557d6523cd4b0b45297fe887da8
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-10 18:16:17 -0400

    Fix button types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React from 'react';
+import { forwardRef, ButtonHTMLAttributes } from 'react';
 
 const baseClasses = 'px-4 py-2 mb-2';
 
@@ -10,11 +10,11 @@ const variantClasses = {
     'text-purple-800 bg-white border border-purple-700 hover:bg-purple-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md'
 };
 
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: keyof typeof variantClasses;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, variant = 'primary', className, ...other }, ref) => (
     <button
       className={classNames(baseClasses, variantClasses[variant], className)}
