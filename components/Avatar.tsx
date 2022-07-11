00b5d10a9f445fecb0a42cc33be97419c3270ec8
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 19:46:01 -0400

    Minor fixes to Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,11 +4,18 @@ import Image, { StaticImageData } from 'next/image';
 interface AvatarProps extends React.HTMLAttributes<HTMLDivElement> {
   src: string | StaticImageData;
   alt?: string;
+  size?: number;
 }
 
-function Avatar({ src, alt, className }: AvatarProps) {
+function Avatar({ src, alt, className, size = 12 }: AvatarProps) {
   return (
-    <div className={classNames('h-12 w-12 rounded-full overflow-hidden relative', className)}>
+    <div
+      className={classNames(
+        `h-${size} w-${size}`,
+        'rounded-full overflow-hidden relative',
+        className
+      )}
+    >
       <Image className="object-cover" src={src} alt={alt} layout="fill" />
     </div>
   );
