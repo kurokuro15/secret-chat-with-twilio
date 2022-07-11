833cc4ad144111f1ea2523f6c2a0d595f94c57a6
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 18:58:38 -0400

    Add ConversationItem component

---
+++ components/ConversationItem.tsx
@@ -0,0 +1,19 @@
+import imgPlaceholder from 'public/photo.jpg';
+import { StaticImageData } from 'next/image';
+import Avatar from './Avatar';
+
+interface ConversationItemProps extends React.HTMLAttributes<HTMLDivElement> {
+  name: string | null;
+  imgSrc?: string | StaticImageData;
+}
+
+function ConversationItem({ name, imgSrc }: ConversationItemProps) {
+  return (
+    <div className="flex gap-5 items-center py-3 px-2 bg-white hover:bg-purple-100 font-bold cursor-pointer">
+      <Avatar src={imgSrc ?? imgPlaceholder} />
+      <div className="grow">{name}</div>
+    </div>
+  );
+}
+
+export default ConversationItem;
