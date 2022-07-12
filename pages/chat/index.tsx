f5656320b4dc61cb231163d488d104af73b1f13f
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 22:40:46 -0400

    Add sidebar to /chat

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -2,43 +2,54 @@ import { NextPage } from 'next';
 import SendIcon from '../../components/icons/SendIcon';
 import Image from 'next/image';
 import perfilTemp from '../../public/photo.jpg';
+import ConversationList from 'components/ConversationList';
+import useConversations from 'hooks/useConversations';
+import ConversationsHeader from 'components/ConversationsHeader';
 
 const Chat: NextPage = () => {
+  const { conversations } = useConversations();
   return (
-    <main id="chat-component" className=" overscroll-contain flex flex-col h-full p-2">
-      <div id="chat-header" className=" flex grow-0 items-center justify-around gap-1">
-        <div id="chat-header-title" className="p-0 m-0">
-          <h1 id="chat" className="p-0 m-0 text-2xl font-bold">
-            Reynaldo González
-          </h1>
-          <small id="status" className="p-0 m-0 text-sm">
-            Online
-          </small>
-        </div>
-        <div className="chat-image p-0 m-0 h-16 rounded-full border aspect-square overflow-hidden flex drop-shadow-md">
-          <Image className="object-cover" src={perfilTemp} alt="profile" />
-        </div>
+    <div className="flex w-full h-full">
+      <div className="w-full max-w-sm border-r border-r-purple-400">
+        <ConversationsHeader />
+        <ConversationList conversations={conversations} />
       </div>
 
-      <div className="grow chat-content"></div>
+      <main id="chat-component" className="grow overscroll-contain flex flex-col h-full p-2">
+        <div id="chat-header" className="flex items-center justify-between">
+          <div id="chat-header-title" className="p-0 m-0">
+            <h1 id="chat" className="p-0 m-0 text-2xl font-bold">
+              Reynaldo González
+            </h1>
+            <small id="status" className="p-0 m-0 text-sm">
+              Online
+            </small>
+          </div>
+          <div className="chat-image p-0 m-0 h-16 rounded-full border aspect-square overflow-hidden flex drop-shadow-md">
+            <Image className="object-cover" src={perfilTemp} alt="profile" />
+          </div>
+        </div>
 
-      <div className="grow-0 inset-x-0 bottom-0 justify-center">
-        <form className="flex container p-0 m-0 gap-x-3">
-          <div className="p-0 m-0 overflow-hidden flex-1">
-            <input
-              className="w-full m-0 border pl-2 pb-1.5 pt-1"
-              type="text"
-              placeholder="Type a message..."
-            />
-          </div>
-          <div className="p-0 m-0 h-9 rounded-full border aspect-square overflow-hidden flex flex-none align-content justify-center hover:bg-slate-400">
-            <button className="w-full" type="submit" aria-label="send">
-              <SendIcon className="w-full h-6" />
-            </button>
-          </div>
-        </form>
-      </div>
-    </main>
+        <div className="grow chat-content"></div>
+
+        <div className="grow-0 inset-x-0 bottom-0 justify-center">
+          <form className="flex container p-0 m-0 gap-x-3">
+            <div className="p-0 m-0 overflow-hidden flex-1">
+              <input
+                className="w-full m-0 border pl-2 pb-1.5 pt-1"
+                type="text"
+                placeholder="Type a message..."
+              />
+            </div>
+            <div className="p-0 m-0 h-9 rounded-full border aspect-square overflow-hidden flex flex-none align-content justify-center hover:bg-slate-400">
+              <button className="w-full" type="submit" aria-label="send">
+                <SendIcon className="w-full h-6" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 };
 
