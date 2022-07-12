6dd298826760e804f8a7243d443db7750515286b
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 22:37:41 -0400

    Wrap app inside AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import 'utils/setupYupLocale';
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout';
+import { AuthProvider } from 'contexts/AuthCtx';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthProvider>
   );
 }
 
