835681d64ba1aed9539ca4df6350c3be5f2d570d
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 23:29:01 -0400

    Improve login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,32 +1,48 @@
-import { useForm } from 'react-hook-form';
-import { object, string } from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { ApiError } from '@supabase/supabase-js';
+import useAuth from 'hooks/useAuth';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Credentials } from 'types/api';
+import { object, string } from 'yup';
+import Alert from './Alert';
 import Button from './Button';
 import Input from './forms/Input';
 import InputFeedback from './forms/InputFeedback';
-import { Credentials } from 'types/api';
-import useAuth from 'hooks/useAuth';
+import Spinner from './Spinner';
 
 const schema = object({
   email: string().email().required(),
   password: string().required()
 });
 
-function LoginForm() {
+export default function LoginForm({ onSuccess }: LoginFormProps) {
   const { signIn } = useAuth();
+  const [error, setError] = useState<ApiError | null>();
 
   const {
     register,
     handleSubmit,
-    formState: { errors, touchedFields }
+    formState: { errors, touchedFields, isSubmitting }
   } = useForm<Credentials>({ resolver: yupResolver(schema) });
 
   async function onSubmit(credentials: Credentials) {
-    console.log(await signIn(credentials));
+    const { error } = await signIn(credentials);
+    if (!error) {
+      onSuccess();
+    }
+
+    setError(error);
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {error && (
+        <Alert variant="danger" className="mb-3">
+          {error.message}
+        </Alert>
+      )}
+
       <label className="block mb-3">
         <div className="mb-1">Correo electrónico</div>
         <Input {...register('email')} isInvalid={!!touchedFields.email && !!errors.email} />
@@ -43,9 +59,13 @@ function LoginForm() {
         <InputFeedback>{errors.password?.message}</InputFeedback>
       </label>
 
-      <Button className="block w-full">Iniciar sesión</Button>
+      <Button className="w-full flex items-center justify-center gap-2" disabled={isSubmitting}>
+        {isSubmitting && <Spinner />} Iniciar sesión
+      </Button>
     </form>
   );
 }
 
-export default LoginForm;
+interface LoginFormProps {
+  onSuccess: () => void;
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,17 @@
 import AppLogo from 'components/icons/AppLogo';
 import LoginForm from 'components/LoginForm';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Login() {
+  const router = useRouter();
+
   return (
     <main className="w-full h-full flex flex-col justify-center max-w-sm mx-auto p-5">
       <AppLogo className="mx-auto" />
       <h1 className="text-center text-2xl font-bold mb-5">SecretChat - Login</h1>
 
-      <LoginForm />
+      <LoginForm onSuccess={() => router.push('/chat')} />
 
       <Link href="/signup">
         <a>¿No tienes cuenta? ¡Regístrate ahora!</a>
