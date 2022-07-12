bfcd63010326fb251a0a3a336a7804c2cf646a4a
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-11 23:28:44 -0400

    Add disabled styles to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,13 +1,13 @@
 import classNames from 'classnames';
 import { forwardRef, ButtonHTMLAttributes } from 'react';
 
-const baseClasses = 'px-4 py-2 mb-2';
+const baseClasses = 'px-4 py-2 mb-2 disabled:cursor-not-allowed';
 
 const variantClasses = {
   primary:
-    'text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md',
+    'text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md disabled:bg-purple-500',
   'outline-primary':
-    'text-purple-800 bg-white border border-purple-700 hover:bg-purple-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md'
+    'text-purple-800 bg-white border border-purple-800 hover:bg-purple-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md disabled:bg-black/10 disabled:text-purple-600 disabled:border-purple-600'
 };
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
