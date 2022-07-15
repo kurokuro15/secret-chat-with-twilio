b97a338d213b5e1436f0f9e124785547d8d47a01
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-15 12:06:48 -0400

    Simplify ChatInput classes

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,28 +1,34 @@
+import Button from './Button';
+import Input from './forms/Input';
 import SendIcon from './icons/SendIcon';
 import SmileIcon from './icons/SmileIcon';
 
 export default function ChatInput() {
   return (
-    <div className="grow-0 inset-x-0 bottom-0 justify-center">
-      <form className="flex container p-0 m-0 gap-x-3">
-        <div className="p-0 m-0 h-9 rounded-full border border-purple-200 aspect-square overflow-hidden flex flex-none align-content justify-center transition-colors touch-auto hover:bg-purple-400">
-          <button className="w-full" type="button" aria-label="send">
-            <SmileIcon className="w-full h-6 text-purple-900" />
-          </button>
-        </div>
-        <div className="p-0 m-0 overflow-hidden flex-1">
-          <input
-            className="w-full m-0 border rounded-xl border-purple-200 pl-2 pb-1.5 pt-1"
-            type="text"
-            placeholder="Type a message..."
-          />
-        </div>
-        <div className="p-0 m-0 h-9 rounded-full border border-purple-200 aspect-square overflow-hidden flex flex-none align-content justify-center transition-colors touch-auto hover:bg-purple-400">
-          <button className="w-full" type="button" aria-label="send">
-            <SendIcon className="w-full h-6 text-purple-900 rotate-90" />
-          </button>
-        </div>
-      </form>
-    </div>
+    <form className="flex gap-x-3 h-9">
+      <Button
+        variant="outline-primary"
+        className="rounded-full p-1 aspect-square border-purple-300"
+        type="button"
+        aria-label="send"
+      >
+        <SmileIcon className="w-6 h-6 mx-auto" />
+      </Button>
+      <div className="grow">
+        <Input
+          className="border rounded-xl border-purple-300"
+          type="text"
+          placeholder="Type a message..."
+        />
+      </div>
+      <Button
+        variant="outline-primary"
+        className="rounded-full p-1 aspect-square border-purple-300"
+        type="button"
+        aria-label="send"
+      >
+        <SendIcon className="w-full rotate-90" />
+      </Button>
+    </form>
   );
 }
--- components/icons/SendIcon.tsx
+++ components/icons/SendIcon.tsx
@@ -1,15 +1,18 @@
 import { SVGProps } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-const SendIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-7 w-7 rotate-90"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    {...props}
-  >
-    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-  </svg>
-);
+const SendIcon = (props: SVGProps<SVGSVGElement>) => {
+  const { className } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={twMerge('h-6 w-6', className)}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+    </svg>
+  );
+};
 
 export default SendIcon;
