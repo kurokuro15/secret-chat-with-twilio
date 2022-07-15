2621bdb356b3a92a0e50d3e105bd7af5d31ea9a5
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-14 22:21:50 -0400

    Move NotificationsProvider to _app.jsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,11 @@
-import { NotificationsProvider } from 'contexts/NotificationsCtx';
 import { ReactNode } from 'react';
 import NotificationsContainer from './NotificationsContainer';
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="h-screen w-screen">
-      <NotificationsProvider>
-        {children}
-        <NotificationsContainer />
-      </NotificationsProvider>
+      {children}
+      <NotificationsContainer />
     </div>
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import 'utils/setupYupLocale';
 import type { AppProps } from 'next/app';
 import Layout from 'components/Layout';
 import { AuthProvider } from 'contexts/AuthCtx';
+import { NotificationsProvider } from 'contexts/NotificationsCtx';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AuthProvider>
+    <NotificationsProvider>
+      <AuthProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
+    </NotificationsProvider>
   );
 }
 
