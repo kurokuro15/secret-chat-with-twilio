ad8bcf248f117e7ecd79e3a755c66149e08533cb
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-15 15:15:10 -0400

    Add black variant to Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,18 @@
 import { twMerge } from 'tailwind-merge';
 import { forwardRef, ButtonHTMLAttributes } from 'react';
 
-const baseClasses = 'px-4 py-2 disabled:cursor-not-allowed transition-all';
+const baseClasses =
+  'px-4 py-2 disabled:cursor-not-allowed transition-all focus:outline-none focus:ring-4 font-medium rounded-md';
 
 const variantClasses = {
   primary:
-    'text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md disabled:bg-purple-500',
+    'text-white bg-purple-700 hover:bg-purple-800 focus:ring-purple-300 disabled:bg-purple-500',
+  black: 'text-white bg-black hover:bg-gray-800 focus:ring-black/30 disabled:bg-gray-600',
   'outline-primary':
-    'text-purple-800 bg-white border border-purple-800 hover:bg-purple-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-md disabled:bg-black/10 disabled:text-purple-600 disabled:border-purple-600',
+    'text-purple-800 bg-white border border-purple-800 hover:bg-purple-800 hover:text-white focus:ring-purple-300 disabled:bg-black/10 disabled:text-purple-600 disabled:border-purple-600',
   'transparent-primary':
-    'p-1 text-purple-800 hover:bg-purple-100 focus:outline-none focus:ring-4 focus:ring-purple-300 disabled:bg-black/10',
-  'transparent-danger':
-    'p-1 text-red-500 hover:bg-red-100 focus:outline-none focus:ring-4 focus:ring-red-300 disabled:bg-black/10'
+    'p-1 text-purple-800 hover:bg-purple-100 focus:ring-purple-300 disabled:bg-black/10',
+  'transparent-danger': 'p-1 text-red-500 hover:bg-red-100 focus:ring-red-300 disabled:bg-black/10'
 };
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
