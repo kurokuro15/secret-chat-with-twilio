935367b372a75d9329d6a0c8fc6c2c8a475d4fdd
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-15 13:03:38 -0400

    Scroll ChatContainer to bottom on start

--- components/ChatConteiner.tsx
+++ components/ChatConteiner.tsx
@@ -1,5 +1,13 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 
 export default function ChatContainer({ children }: { children: ReactNode } = { children: null }) {
-  return <div className="grow overflow-y-auto rounded-2xl my-2">{children}</div>;
+  const divRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    divRef.current?.scrollTo({ top: divRef.current.scrollHeight });
+  });
+  return (
+    <div ref={divRef} className="grow overflow-y-auto rounded-2xl my-2">
+      {children}
+    </div>
+  );
 }
