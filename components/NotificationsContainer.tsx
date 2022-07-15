abef4fdf86e16a1da83f38b14738b3086fcb474a
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-14 20:35:39 -0400

    Fix bug

--- components/NotificationsContainer.tsx
+++ components/NotificationsContainer.tsx
@@ -5,7 +5,7 @@ function NotificationsContainer() {
   const { notifications } = useNotificationsCtx();
 
   return (
-    <div className="w-full max-w-sm p-5 fixed top-0 left-1/2 -translate-x-1/2 md:top-full md:left-full md:-translate-y-full md:-translate-x-full">
+    <div className="w-full max-w-sm p-5 md:pt-0 fixed top-0 left-1/2 -translate-x-1/2 md:top-full md:left-full md:-translate-y-full md:-translate-x-full">
       <div className="flex flex-col gap-3">
         {notifications.map((notification) => (
           <Notification key={notification.id} {...notification} />
