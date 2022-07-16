eebec1aa06ba5f022c9d8652c42454bf9271fbef
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-15 23:15:09 -0400

    Add user metadata to useAuth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,11 +1,13 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import setAuthToken from 'services/setAuthToken';
 import { Credentials } from 'types/api';
 import { supabase } from 'utils/supabaseClient';
 
 function useAuth() {
   const router = useRouter();
+  const user = supabase.auth.user();
+  const [userData, setUserData] = useState<Partial<UserData>>({});
 
   async function signIn(credentials: Credentials) {
     return await supabase.auth.signIn(credentials);
@@ -26,18 +28,51 @@ function useAuth() {
     router.push('/login');
   }
 
+  async function updateUserData(userData: Partial<UserData>) {
+    const result = await supabase
+      .from('profiles')
+      .update({ ...userData })
+      .eq('id', user?.id);
+
+    if (!result.error) {
+      setUserData(userData);
+    }
+
+    return result;
+  }
+
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (event, session) => await setAuthToken(event, session));
   }, []);
+
+  useEffect(() => {
+    async function getUserData() {
+      const { data } = await supabase
+        .from('profiles')
+        .select('username, avatar_url')
+        .eq('id', user?.id);
+      setUserData(data && data[0]);
+    }
+    getUserData();
+  }, [user]);
 
   return {
     signIn,
     signInWithGithub,
     signUp,
     signOut,
+    updateUserData,
     jwt: supabase.auth.session()?.access_token,
-    user: supabase.auth.user()
+    user: {
+      ...user,
+      ...userData
+    }
   };
 }
 
 export default useAuth;
+
+interface UserData {
+  username: string;
+  avatar_url: string;
+}
