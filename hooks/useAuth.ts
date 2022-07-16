c3321e6dff03e828cdba4e1bd4976c2161529ff9
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-16 01:09:52 -0400

    Get user avatar on start

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import setAuthToken from 'services/setAuthToken';
 import { Credentials } from 'types/api';
 import { supabase } from 'utils/supabaseClient';
@@ -7,7 +7,7 @@ import { supabase } from 'utils/supabaseClient';
 function useAuth() {
   const router = useRouter();
   const user = supabase.auth.user();
-  const [userData, setUserData] = useState<Partial<UserData>>({});
+  const [userData, setUserData] = useState<Partial<UserData>>();
 
   async function signIn(credentials: Credentials) {
     return await supabase.auth.signIn(credentials);
@@ -35,11 +35,29 @@ function useAuth() {
       .eq('id', user?.id);
 
     if (!result.error) {
-      setUserData(userData);
+      if (userData.avatar_url) {
+        const avatar_url = await getAvatar(userData.avatar_url);
+        setUserData((prev) => ({ ...prev, ...userData, avatar_url }));
+      } else {
+        setUserData((prev) => ({ ...prev, ...userData }));
+      }
     }
 
     return result;
   }
+
+  const getAvatar = useCallback(
+    async (url: string) => {
+      const { data } = await supabase.from('profiles').select('avatar_url').eq('id', user?.id);
+
+      if (data && data[0].avatar_url) {
+        const { data: img } = await supabase.storage.from('avatars').download(url);
+        const avatarUrl = img && URL.createObjectURL(img);
+        return avatarUrl ?? undefined;
+      }
+    },
+    [user?.id]
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (event, session) => await setAuthToken(event, session));
@@ -51,10 +69,14 @@ function useAuth() {
         .from('profiles')
         .select('username, avatar_url')
         .eq('id', user?.id);
-      setUserData(data && data[0]);
+
+      if (data && data[0].avatar_url) {
+        const avatar_url = await getAvatar(data[0].avatar_url);
+        setUserData(data && { ...data[0], avatar_url });
+      }
     }
     getUserData();
-  }, [user]);
+  }, [user, getAvatar]);
 
   return {
     signIn,
@@ -62,11 +84,14 @@ function useAuth() {
     signUp,
     signOut,
     updateUserData,
+    getAvatar,
     jwt: supabase.auth.session()?.access_token,
-    user: {
-      ...user,
-      ...userData
-    }
+    user: userData
+      ? {
+          ...user,
+          ...userData
+        }
+      : null
   };
 }
 
