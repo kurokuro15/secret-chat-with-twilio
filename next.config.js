47ecb5ff1879d7fa20046a833496c0609ec510e8
kurokuro15 <kurokuro15000@gmail.com>
2022-07-19 18:07:02 -0400

    fix error for http prefix.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL]
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL.replace(/https:\/\//, '')]
   }
 };
 
