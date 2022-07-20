7a5972bfc6a2e25ae85522391aac36d5de9a55a2
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-20 15:15:58 -0400

    Increment NotificationsContainer z-index

--- components/ui/NotificationsContainer.tsx
+++ components/ui/NotificationsContainer.tsx
@@ -5,7 +5,7 @@ function NotificationsContainer() {
   const { notifications } = useNotifications();
 
   return (
-    <div className="w-full max-w-sm px-5 pt-2 lg:pt-0 lg:pb-2 fixed top-0 left-1/2 -translate-x-1/2 lg:top-full lg:left-full lg:-translate-y-full lg:-translate-x-full">
+    <div className="w-full max-w-sm px-5 pt-2 lg:pt-0 lg:pb-2 fixed top-0 left-1/2 -translate-x-1/2 lg:top-full lg:left-full lg:-translate-y-full lg:-translate-x-full z-50">
       <div className="flex flex-col gap-3">
         {notifications.map((notification) => (
           <Notification key={notification.id} {...notification} />
