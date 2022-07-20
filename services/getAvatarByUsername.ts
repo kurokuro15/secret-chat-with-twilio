637e4adbc0b1f4f9e69b3c44da743933c00bc6a9
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-19 22:39:16 -0400

    Fix bug

--- services/getAvatarByUsername.ts
+++ services/getAvatarByUsername.ts
@@ -4,6 +4,9 @@ export default async function getAvatarByUsername(username: string) {
   const { data } = await supabase.from('profiles').select('avatar_url').eq('username', username);
   if (data && data[0]) {
     const userAvatarUrl = data[0].avatar_url;
-    return supabase.storage.from('avatars').getPublicUrl(userAvatarUrl).publicURL || null;
+    return userAvatarUrl
+      ? supabase.storage.from('avatars').getPublicUrl(userAvatarUrl).publicURL
+      : null;
   }
+  return null;
 }
