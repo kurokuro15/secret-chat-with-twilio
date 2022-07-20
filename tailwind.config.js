bf1fb65894a31cc8d15c48b956408e3e0f129bfa
kurokuro15 <kurokuro15000@gmail.com>
2022-07-20 15:41:58 -0400

    add Roboto as  primary sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily : {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
       gridTemplateColumns: {
         auto: 'repeat(auto-fill, minmax(1.85em, 1fr))'
       }
