725425ff5abf9375a5cff3851526230446a820d5
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-21 12:33:22 -0400

    Specify fetch to supabase

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,4 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!, {
+  fetch: (...args) => fetch(...args)
+});
