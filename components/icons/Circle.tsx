98bfb9bd51620f8c979d43c78e2bd150f09f7bd8
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-20 20:46:00 -0400

    Minor change to Circle

--- components/icons/Circle.tsx
+++ components/icons/Circle.tsx
@@ -5,7 +5,7 @@ export default function Circle(props: HTMLProps<HTMLSpanElement>) {
   return (
     <span
       {...props}
-      className={twMerge('grow-0 m-1 aspect-square w-4 h-4 rounded-full border-2', props.className)}
+      className={twMerge('grow-0 aspect-square w-4 h-4 rounded-full border-2', props.className)}
     ></span>
   );
 }
