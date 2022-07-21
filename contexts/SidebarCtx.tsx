f158974af891aaace81484e20528dd77c6c41a07
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-21 00:10:52 -0400

    Fix bug where the screen went all blank

--- contexts/SidebarCtx.tsx
+++ contexts/SidebarCtx.tsx
@@ -1,11 +1,13 @@
-import React, { ReactNode, useCallback, useState } from 'react';
+import React, { ReactNode, useCallback, useEffect, useState } from 'react';
 import { Panel } from 'components/ChatSidebar';
+import { useConversations } from 'hooks';
 
 interface SidebarCtx {
   show: boolean;
   toggleSidebar: () => void;
   currentPanel: Panel;
   changePanel: (panel: Panel) => void;
+  isMobile: boolean;
 }
 
 const SidebarCtx = React.createContext<SidebarCtx | undefined>(undefined);
@@ -13,9 +15,29 @@ const SidebarCtx = React.createContext<SidebarCtx | undefined>(undefined);
 function SidebarProvider({ children }: { children: ReactNode }) {
   const [show, setShow] = useState(true);
   const [currentPanel, setCurrentPanel] = useState<Panel>('conversations');
+  const [isMobile, setIsMobile] = useState(false);
+
+  const { selectedConversation } = useConversations();
 
   const toggleSidebar = useCallback(() => setShow((prev) => !prev), []);
   const changePanel = useCallback((panel: Panel) => setCurrentPanel(panel), []);
+
+  useEffect(() => {
+    function checkWidth() {
+      setIsMobile(window.innerWidth < 1024);
+    }
+    window.addEventListener('resize', checkWidth);
+
+    if (!selectedConversation) {
+      setShow(true);
+    } else {
+      if (isMobile) {
+        setShow(false);
+      }
+    }
+
+    return () => window.removeEventListener('resize', checkWidth);
+  }, [selectedConversation, isMobile]);
 
   return (
     <SidebarCtx.Provider
@@ -23,7 +45,8 @@ function SidebarProvider({ children }: { children: ReactNode }) {
         show,
         toggleSidebar,
         currentPanel,
-        changePanel
+        changePanel,
+        isMobile
       }}
     >
       {children}
