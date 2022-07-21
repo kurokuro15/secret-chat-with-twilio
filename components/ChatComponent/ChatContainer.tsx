8562c23d507085ea616e35064edbd474f5c0dba8
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-07-21 11:10:41 -0400

    Add infinite scroll

--- components/ChatComponent/ChatContainer.tsx
+++ components/ChatComponent/ChatContainer.tsx
@@ -1,26 +1,58 @@
 import { Conversation, Message, Paginator } from '@twilio/conversations';
 import { useAuth } from 'hooks';
+import throttle from 'just-throttle';
 import { useEffect, useRef, useState, useTransition } from 'react';
 import getAvatarByUsername from 'services/getAvatarByUsername';
 import ChatMessageContainer from './ChatMessangeContainer';
 
 export default function ChatContainer({ conversation }: { conversation: Conversation }) {
   const divRef = useRef<HTMLDivElement>(null);
+  const visorRef = useRef<HTMLDivElement>(null);
   const [messages, setMessages] = useState<Message[]>([]);
   const [avatars, setAvatars] = useState<Record<string, string>>({});
   const { user } = useAuth();
   const [gotMessages, startSettingMessages] = useTransition();
+  const paginator = useRef<Paginator<Message>>();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      throttle(() => {
+        const currentPaginator = paginator.current;
+        if (!currentPaginator) {
+          conversation.getMessages(30).then((newPaginator) => {
+            paginator.current = newPaginator;
+            startSettingMessages(() => setMessages((prev) => [...newPaginator.items, ...prev]));
+          });
+        } else {
+          if (currentPaginator.hasPrevPage) {
+            currentPaginator.prevPage().then((newPaginator) => {
+              paginator.current = newPaginator;
+              setMessages((prev) => [...newPaginator.items, ...prev]);
+            });
+          }
+        }
+      }, 2000),
+      {
+        root: divRef.current,
+        threshold: 1,
+        rootMargin: '35%'
+      }
+    );
+
+    visorRef.current && observer.observe(visorRef.current);
+
+    return () => {
+      observer.disconnect();
+      paginator.current = undefined;
+      setMessages([]);
+    };
+  }, [conversation]);
 
   useEffect(() => {
     if (!gotMessages) {
       divRef.current?.scrollTo({ top: divRef.current.scrollHeight, behavior: 'smooth' });
     }
   }, [gotMessages]);
-
-  useEffect(() => {
-    const author = messages[messages.length - 1]?.author;
-    if (author === user?.username) divRef.current?.scrollTo({ top: divRef.current.scrollHeight });
-  }, [messages, user]);
 
   useEffect(() => {
     const getAvatarUrl = async (identity: string | null) => {
@@ -44,54 +76,32 @@ export default function ChatContainer({ conversation }: { conversation: Conversa
       return Object.fromEntries(avatarsUrls);
     };
 
-    const getMsg = async () => {
-      const pageSize = 20;
-      let allMessages: Array<Message> = [];
-
-      const collectMessages = async (paginator: Paginator<Message>) => {
-        try {
-          let end = false;
-          do {
-            const messages = paginator.items;
-            allMessages = [...messages, ...allMessages];
-            if (paginator.hasPrevPage) {
-              paginator = await paginator.prevPage();
-            } else {
-              end = true;
-            }
-          } while (!end);
-          startSettingMessages(() => {
-            setMessages(allMessages);
-          });
-        } catch (error) {
-          console.error(error);
-          return [];
-        }
-      };
-
-      conversation
-        .getMessages(pageSize)
-        .then((messages) => collectMessages(messages))
-        .then().catch((error) => console.error(error));
-    };
-
     getAllAvatarUrl(conversation).then((avatarsUrls) => {
       setAvatars(avatarsUrls);
-      getMsg();
     });
 
     function addMessage(message: Message) {
-      setMessages((messages) => [...messages, message]);
+      let atBottom;
+      const container = divRef.current;
+      if (container?.scrollHeight && container?.scrollTop) {
+        atBottom = container?.scrollHeight - container?.scrollTop === container?.clientHeight;
+      }
+      if (message.author === user?.username || atBottom) {
+        startSettingMessages(() => setMessages((messages) => [...messages, message]));
+      } else {
+        setMessages((messages) => [...messages, message]);
+      }
     }
     conversation.on('messageAdded', addMessage);
 
     return () => {
       conversation.removeListener('messageAdded', addMessage);
     };
-  }, [conversation]);
+  }, [conversation, user]);
 
   return (
     <div ref={divRef} className="grow overflow-y-auto snap-y scroll-smooth" tabIndex={0}>
+      <div ref={visorRef} />
       {messages.map((message) => {
         const avatarUrl: string | null = message.author ? avatars[message.author] ?? null : null;
         const msg: iMessenge = {
